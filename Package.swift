b81e9260ecbbccca06b15a579855a4210aad9bf3
Shep Moore-Berg <theshepster@gmail.com>
2016-08-21 12:32:34 -0400

    SwiftyJSON 2.2.0 and higher
    Instead of 2.3.3 and higher, like the last accidental commit

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@ import PackageDescription
 let package = Package(
     name: "telegram-bot-swift",
     dependencies: [
-      .Package(url: "https://github.com/zmeyc/SwiftyJSON.git", versions: "2.3.3" ..< Version.max)
+      .Package(url: "https://github.com/zmeyc/SwiftyJSON.git", versions: "2.2.0" ..< Version.max)
     ]
 )
 
