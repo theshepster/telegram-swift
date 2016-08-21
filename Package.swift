6355793e8bd5c74fe425c678cc863f706396c157
Shep Moore-Berg <theshepster@gmail.com>
2016-08-21 12:54:58 -0400

    Updating to theshepster's SwiftyJSON

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@ import PackageDescription
 let package = Package(
     name: "telegram-bot-swift",
     dependencies: [
-      .Package(url: "https://github.com/zmeyc/SwiftyJSON.git", majorVersion: 10, minor: 0)
+      .Package(url: "https://github.com/theshepster/SwiftyJSON.git", versions: "2.2.0" ..< Version.max)
     ]
 )
 
